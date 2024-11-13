ac79aad7f13810ed30688a00056d07d7274f5e92
hassan <hassanibrahimndahiro@gmail.com>
2024-11-13 15:24:25 +0300

    board solved

---
+++ Battleship-Group7/src/Components/Board.jsx
@@ -0,0 +1,28 @@
+
+
+
+
+
+import React from "react";
+import Cell from "./Cell.jsx";
+
+function Board({ board, onCellClick, isPlayer }) {
+  return (
+    <div className="board">
+      {board.map((row, rowIndex) =>
+        row.map((cell, colIndex) => (
+          <Cell
+            key={${rowIndex}-${colIndex}}
+            x={rowIndex}
+            y={colIndex}
+            cell={cell}
+            onClick={onCellClick}
+            isPlayer={isPlayer}
+          />
+        ))
+      )}
+    </div>
+  );
+}
+
+export default Board;
