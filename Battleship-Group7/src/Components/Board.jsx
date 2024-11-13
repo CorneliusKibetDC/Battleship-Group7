dcba68506d5687b1898167094de557645deebf9a
hassan <hassanibrahimndahiro@gmail.com>
2024-11-13 16:02:46 +0300

    added main component

--- Battleship-Group7/src/Components/Board.jsx
+++ Battleship-Group7/src/Components/Board.jsx
@@ -8,7 +8,19 @@ import Cell from "./Cell.jsx";
 
 function Board({ board, onCellClick, isPlayer }) {
   return (
-    <div className="board">
+    <div className="board" style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${board.length}, 1fr)`,
+        gridTemplateRows: `repeat(${board.length}, 1fr)`,
+        gap: "1px",
+        width: "400px",
+        height: "400px",
+        border: "1px solid black",
+        backgroundColor: "lightgray",
+        boxSizing: "border-box",
+        padding: "10px",
+  
+    }}>
       {board.map((row, rowIndex) =>
         row.map((cell, colIndex) => (
           <Cell
